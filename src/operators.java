ccd4aefc3420f669f84977a84bf8116ac28c3fdf
jasper <whiteana001@warren.k12.in.us>
2024-08-07 13:47:48 -0400

    Initial Commit

--- src/operators.java
+++ src/operators.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class operators {
+public class operators {
+
+    public static void main(String[] args) {
+        double numOne = 20.00;
+        double numTwo = 80.00;
+        double numThree = (numOne + numTwo) * 100;
+        double  remainder = numThree % 40.00;
+        boolean value = false;
+
+        if(!value){
+            System.out.println("got some remainder");
+        }
 }
+    }
+
+
+
+
